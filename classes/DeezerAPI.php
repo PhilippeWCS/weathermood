d0d1a0a59383195bbc3021b65851de29ef3a2b97
PhilippeWCS <philippedebridiers@gmail.com>
2017-03-30 13:29:13 +0200

    Mise en place search method API ; Creation deezer.php

--- classes/DeezerAPI.php
+++ classes/DeezerAPI.php
@@ -12,4 +12,44 @@ namespace classes;
 class DeezerAPI
 {
 
+    private static $apiUrl = "https://api.deezer.com/";
+    private static $currentURL = "";
+
+    public function __construct(){
+        self::$currentURL = self::$apiUrl;
+    }
+
+    public function search($search){
+
+        if (isset($search)){
+            self::$currentURL .=  "search/playlist?q=" . $search;
+
+            return $this->getData();
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    private function getData(){
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => self::$currentURL,
+            CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+        ));
+
+        $data = curl_exec($curl);
+        curl_close($curl);
+
+        return $data;
+
+    }
+
+
+
+
 }
---
+++ deezer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 30/03/17
+ * Time: 11:26
+ */
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    require_once "classes/DeezerAPI.php";
+
+    $apiDeezer = new \classes\DeezerAPI();
+    $data = json_decode($apiDeezer->search("sun"));
+
+    $playlist = $data->data[0]->id;
+    var_dump($playlist);
