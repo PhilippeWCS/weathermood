618f17171715a84b587c7de625e8a3191902eb46
Alexandre Leuthreau <leuthreau.alexandre@gmail.com>
2017-03-30 18:00:09 +0200

    Amélioration css

--- index.php
+++ index.php
@@ -1,56 +1,46 @@
-<?php>
-
-
-<>
-
 <!DOCTYPE html>
-<html>
+    <html>
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE = edge">
-    <meta name="viewport" content="width = device-width, initial-scale = 1, maximum-scale=1, user-scalable=no">
-    <link href="appweather.css" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE = edge">
+        <meta name="viewport" content="width = device-width, initial-scale = 1, maximum-scale=1, user-scalable=no">
+        <link href="appweather.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <link href="https://fonts.googleapis.com/css?family=Rationale" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Rationale" rel="stylesheet">
 
 
 
-</head>
+    </head>
 
-<body>
-    <div class="container-fluid">
+    <body class="container-fluid">
+        <img src="http://whd.erth.biz/wp-content/uploads/nature/Sky-colors-weather-clouds-landscape-high-resolution-1600x900.jpg" />
         <div class="city-bar">
-            <div class="row no-gutters">
-                <div class="col-xs-offset-4 col-xs-4 col-xs-offset-4">
-                    <form>
-                        <div class="form-group">
-                            <label for="formGroupExampleInput"><h1>CITY</h1></label>
-                            <input type="text" class="form-control" id="formGroupExampleInput" name="searchcity" placeholder="City ...">
+                <div class="row no-padding">
+                    <div class="col-xs-offset-4 col-xs-4 col-xs-offset-4">
+                        <div class="title">
+                        <h1>WEATHERMOOD</h1>
                         </div>
-                        <div class="button">
-                            <a><button type="button" class="btn btn-primary">SEARCH &raquo;</button></a>
-                        </div>
-                    </form>
+                        <form action="" method="get">
+                            <div class="form-group">
+                                <label for="formGroupExampleInput"><h2>CITY</h2></label>
+                                <input type="text" class="form-control" id="formGroupExampleInput" name="searchcity" placeholder="City ...">
+                            </div>
+                            <div class="button">
+                                <button type="submit" class="btn btn-primary">SEARCH &raquo;</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="container-fluid">
-        <div class="music-player">
-            <iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=playlist&id=30595446&app_id=1"
-                width="700" height="350"></iframe>
-        </div>
-    </div>
 
 
 
-</body>
 
-</html>
+    </body>
+
+    </html>
